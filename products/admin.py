e082820bdbcf71c066797f567f407538ebbdc38d
Eric Blake <blake.eric@gmail.com>
2024-01-18 12:20:30 +0000

    Add product admin, migrate changes

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'country',
+        'vintage',
+        'price',
+        'image',
+    )
+
+    ordering = ('price',)
+
+admin.site.register(Product, ProductAdmin)
