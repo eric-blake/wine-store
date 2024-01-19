37f87f8661253c2bde4de19d19688ffb2cb0fba8
Eric Blake <blake.eric@gmail.com>
2024-01-19 20:23:42 +0000

    Add products to home page template

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name ='home'),
-    path('products/', include('products.urls')),
+ 
 ]
