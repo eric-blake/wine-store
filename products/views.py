d7f2c3783b9b229305d72d8f978144c6fe6384a4
eric-blake <129101556+eric-blake@users.noreply.github.com>
2024-01-22 21:27:36 +0000

    Update filtering logic

--- products/views.py
+++ products/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views import generic, View
-from .models import Product, Colour
+from .models import Product, Colour, Style, Grape
 from django.db.models import Q
+from django.contrib import messages
+
 
 def all_products(request):
     """ A view to show all products"""
@@ -9,12 +11,37 @@ def all_products(request):
     products = Product.objects.all().order_by('-created')
     query = None
     colours = None
+    styles = None
+    grapes = None
+    country = None
 
     if request.GET:
+
+        if 'country' in request.GET:
+            countries = request.GET['country'].split(',')
+            products = products.filter(country__name__in=countries)
+            countries = country.objects.filter(name__in=countries)
+
+
+
         if 'colour' in request.GET:
             colours = request.GET['colour'].split(',')
             products = products.filter(colour__name__in=colours)
             colours = Colour.objects.filter(name__in=colours)
+
+
+        if 'style' in request.GET:
+            styles = request.GET['style'].split(',')
+            products = products.filter(style__name__in=styles)
+            styles = Style.objects.filter(name__in=styles)
+
+            print(styles)
+
+
+        if 'grape' in request.GET:
+            grapes = request.GET['grape'].split(',')
+            products = products.filter(grape__name__in=grapes)
+            grapes = Grape.objects.filter(name__in=grapes)
 
         if 'q' in request.GET:
                 query = request.GET['q']
@@ -29,6 +56,9 @@ def all_products(request):
     context = {
             'products': products,
             'search_term': query,
+            'current_colours': colours,
+            'current_styles': styles,
+            'current_grapes': grapes,
         }
 
     return render(request, 'products/products.html', context)
