eb4fcc3b0bc6ac15455c2aacd11a727378c07f45
eric-blake <129101556+eric-blake@users.noreply.github.com>
2024-01-24 18:38:56 +0000

    Update shopping bag template

--- bag/views.py
+++ bag/views.py
@@ -8,7 +8,7 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
-
+   
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -19,5 +19,5 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
+    
