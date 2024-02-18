3a1f925a7cab668aaa98637db54ffd1bd77226b0
eric-blake <129101556+eric-blake@users.noreply.github.com>
2024-02-18 20:52:46 +0000

    Fix issue with email error

--- wine_store/settings.py
+++ wine_store/settings.py
@@ -31,8 +31,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+
 
 ALLOWED_HOSTS = ['127.0.0.1:8000','127.0.0.1','wine-store-ci-bbef57464b33.herokuapp.com','localhost']
 
