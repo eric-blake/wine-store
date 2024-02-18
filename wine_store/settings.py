d8ce964a9b5ff5a76ab5ceb90cb024e182ba5e98
eric-blake <129101556+eric-blake@users.noreply.github.com>
2024-02-18 17:50:50 +0000

    Update email settings

--- wine_store/settings.py
+++ wine_store/settings.py
@@ -33,8 +33,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-# DEBUG = True
-
 ALLOWED_HOSTS = ['127.0.0.1:8000','127.0.0.1','wine-store-ci-bbef57464b33.herokuapp.com','localhost']
 
 
@@ -246,5 +244,5 @@ else:
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
