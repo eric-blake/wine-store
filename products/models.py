104987af07a37111606ed28e6a2dc47361dc65a2
eric-blake <129101556+eric-blake@users.noreply.github.com>
2024-02-20 19:31:36 +0000

    Add favourites model

--- products/models.py
+++ products/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django_countries.fields import CountryField
+from django.contrib.auth.models import User
+from profiles.models import UserProfile
 
 class Product(models.Model):
     title = models.CharField(max_length = 254)
@@ -68,3 +70,12 @@ class Grape(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Favourites(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.title
