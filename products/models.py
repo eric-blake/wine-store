c9aa6ec94ddac63b9d787d379c075f2bf9f0eb40
eric-blake <129101556+eric-blake@users.noreply.github.com>
2024-03-19 22:46:19 +0000

    Add freindly name to product model

--- products/models.py
+++ products/models.py
@@ -97,6 +97,8 @@ class Grape(models.Model):
         verbose_name_plural = 'Grapes'
 
     name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
 
     def __str__(self):
         return self.name
